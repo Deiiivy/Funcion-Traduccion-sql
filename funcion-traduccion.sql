ca5ad59e4c0f6ee81d5db27c633488d1aea5bb0d
Deiiivy <deivypr28@gmail.com>
2025-04-16 19:41:41 -0500

    changes

--- funcion-traduccion.sql
+++ funcion-traduccion.sql
@@ -1,2 +1,17 @@
+CREATE FUNCTION dbo.TraducirFrase
+(
+    @Frase NVARCHAR(MAX),
+    @IdIdioma INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @FraseTraducida NVARCHAR(MAX) = @Frase;
 
-x
+    SELECT @FraseTraducida = REPLACE(@FraseTraducida, f.Texto, ISNULL(t.Texto, f.Texto))
+    FROM Frase f
+    LEFT JOIN Traduccion t ON t.IdFrase = f.Id AND t.IdIdioma = @IdIdioma;
+
+    RETURN @FraseTraducida;
+END;
+GO
