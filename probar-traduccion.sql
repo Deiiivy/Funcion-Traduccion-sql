49ff8e8d61b4161c160e282fe19d963d621f7bb9
Deiiivy <deivypr28@gmail.com>
2025-04-16 19:39:56 -0500

    Initializing

---
+++ probar-traduccion.sql
@@ -0,0 +1,9 @@
+--probar
+USE Traduccion;
+
+SELECT dbo.TraducirFrase('Hola Mundo', 2); 
+SELECT dbo.TraducirFrase('Buenos días', 2); 
+SELECT dbo.TraducirFrase('Buenas Noches', 2); 
+SELECT dbo.TraducirFrase('Hola Mundo', 3); 
+SELECT dbo.TraducirFrase('Buenos días', 3); 
+
